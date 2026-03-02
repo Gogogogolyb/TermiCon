011b0253980fd905d4cf10c1b243b6af6e4a4bd6
Usatblu27 <daniilusatenko2772@gmail.com>
2026-03-02 21:33:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,50 @@
-const CACHE_NAME = "v1";
-const urlsToCache = [
+// const CACHE_NAME = "v1";
+// const urlsToCache = [
+//   "/",
+//   "/index.html",
+//   "/icons/icon-192.png",
+//   "/icons/icon-512.png",
+// ];
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches
+//       .match(event.request)
+//       .then((response) => response || fetch(event.request)),
+//   );
+// });
+const CACHE_NAME = "Glow-Flow-v2";
+
+const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
 ];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
-  );
+  console.log("Service Worker installing...");
+  event.waitUntil(caches.open(CACHE_NAME).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      })(),
+    );
+    return;
+  }
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request)),
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
   );
 });
